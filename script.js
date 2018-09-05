c175f33c5f99b90359d578671a4eb3093eeaf7bd
Moon-Tae Kim <moon-tae@Moon-Taes-MacBook-Air.local>
2018-09-05 17:19:25 -0400

    functional game

--- script.js
+++ script.js
@@ -1,48 +1,41 @@
-// const capitolArray = ["ATLANTA", "CHICAGO", "PHEONIX", "JACKSON", "TRENTON"]
+const capitolArray = ["ATLANTA", "CHICAGO", "PHEONIX", "JACKSON", "TRENTON"]
 
-// const createRandomCapitol = () => {
+const createRandomCapitol = () => {
 
-//     const randomCapitolNumber = Math.floor(Math.random() * capitolArray.length)
-//     const randomCapitol = capitolArray[randomCapitolNumber]
-//     return randomCapitol.split('')
-
-//     //$(`<div>${randomCapitol}</div>`).appendTo('.userLetters')
-//     //now add this capitolDiv to the HTML
-// }   
-
-// //createRandomCapitol()
-// console.log(createRandomCapitol())
-
-// //var letterSelect = console.log(document.querySelector('.letterList').innerText.split(''))
-// var letterSelect = console.log($('.letterList').text().split(''))
+    const randomCapitolNumber = Math.floor(Math.random() * capitolArray.length)
+    const randomCapitol = capitolArray[randomCapitolNumber]
+    return randomCapitol
 
 
+    //$(`<div>${randomCapitol}</div>`).appendTo('.userLetters')
+    //now add this capitolDiv to the HTML
+}   
 
+//createRandomCapitol()
+
+
+//var letterSelect = console.log(document.querySelector('.letterList').innerText.split(''))
+//var letterSelect = console.log($('.letterList').text().split(''))
 
 
 
 
-
-
-
-var word = "WORLD"
-var wordArr = word.split('')
+var word = createRandomCapitol()
+console.log(word)
 
 $(() => {
 
     
-    //take wordArr and insert divs = to length of wordArr
-    for(let i = 0; i < wordArr.length; i++){
-        $newDiv = $('<div ></div>', {'class': wordArr[i], text: '_' } )
-       // $newDiv = $('<div ></div>', {'class': wordArr[i], text: wordArr.indexOf(wordArr[i]) } )
+//take wordArr and insert divs = to length of wordArr
+    for(let i = 0; i < word.length; i++){
+        $newDiv = $('<div></div>', {'class': word[i], text:'_' } )
         $('.userLetters').append($newDiv)
-
     }  
 
-    //losing function//
+//losing function//
     $('.letterList').on('click', ($event) => {
         var letterSelect = $event.target.innerHTML
-        if(word.indexOf(letterSelect.toLowerCase()) === -1){
+        if(word.indexOf(letterSelect) === -1){
             var life = $('.life').text()
             $('.life').html(`${life -= 1}`)
         
@@ -54,49 +47,33 @@ $(() => {
 
  //winning funciton//
     $('.letterList').on('click', () => {
-        var letters = $('.userLetters').children().text()
+        var letters = $('.userLetters').text()
         var finishMessage = $('.finishMessage').text()
-        if (letters === word) {
+        if (letters == word) {
             $('.finishMessage').html("YOU WIN YOU BIG LOSER")
-            alert("you win")
+           // alert("you win")
         }
     })
 })
 
-    //letters selected//
-    // $('.letterList').on('click', ($letterSelected) => {
-    //     var letterSelected = $letterSelected.target.innerHTML
-    //     if(letterSelected = valueofclass...)
-    //     $('.letterList')
+//letters selected//
+    $('.letterList').on('click', ($letterSelected) => {
+        var letterSelected = $letterSelected.target.innerHTML
 
-    // })
+        if(word.includes(letterSelected) === true){
+            $(`.${letterSelected}`).replaceWith(`${letterSelected}`) 
 
-// $('.A').on('click', ()=>{
-//         //console.log('user selected A')
-//     $('.firstLetter, .fourthLetter, .seventhLetter').replaceWith($('.A'));
-//     win()
-//     lose()
-//     //the win and loss function should be called in each of these
+        }
+    })
 
-// })
 
-// $('.T').on('click', ()=>{
-//        // console.log('user selected T')
-//     $('.secondLetter, .sixthLetter').replaceWith($('.T'));
-//    win()
-//    lose()
-// })
 
-// $('.L').on('click', ()=>{
-//         //console.log('user selected L')
-//     $('.thirdLetter').replaceWith($('.L'));
-//    win()
-//    lose()
-// })
 
-// $('.N').on('click', ()=>{
-//        // console.log('user selected N')
-//      $('.fifthLetter').replaceWith($('.N'));
-//     win()
-//     lose()
-// })
+
+
+
+
+
+
+
+
