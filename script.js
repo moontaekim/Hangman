256ffc22ff4ceb24421d127422c0ff6d5cf2fb0e
Moon-Tae Kim <moon-tae@Moon-Taes-MacBook-Air.local>
2018-09-06 19:04:15 -0400

    Styling almost done

--- script.js
+++ script.js
@@ -1,31 +1,65 @@
-//const marvelHerosArray = ["ANTMAN", "IRONMAN", "HULK", "BLACKWIDOW", "SPIDERMAN", "GROOT", "STARLORD", "BLACKPANTHER", "VISION", "THOR", "CAPTAINAMERICA"]
-const marvelHerosArray = [
-    {hero: "ANTMAN", img: "https://vignette.wikia.nocookie.net/deathbattlefanon/images/c/ce/Ant-Man_Render.png/revision/latest?cb=20160514061120"},
-    {hero: "IRONMAN", img:"http://static.tumblr.com/5a7f2db3663f6b7a24edf8efa35c0b0f/povhbe1/JlHmn510g/tumblr_static_ironman_3.png"},
-    {hero: "HULK", img: "https://clipart.info/images/ccovers/1516942386The-Incredible-Hulk-Png-Hd-Big.png"},
-    {hero:"BLACKWIDOW", img: "http://pluspng.com/img-png/black-widow-png-download-black-widow-png-images-transparent-gallery-advertisement-1473.png"},
-    {hero: "SPIDERMAN", img: "https://purepng.com/public/uploads/large/purepng.com-amazing-spidermanspider-manspidermansuperherocomic-bookmarvel-comicscharacterstan-leegamesmovie-1701528655908xx8zi.png"},
-    {hero: "GROOT", img: "https://vignette.wikia.nocookie.net/lapis/images/f/f4/Groot_Movie_Alt.png/revision/latest?cb=20160315105655"},
-    {hero: "STARLORD", img: "https://vignette.wikia.nocookie.net/disney/images/9/95/Star-Lord_Promo_Art_Decor_III.png/revision/latest?cb=20140625043807"},
-    {hero: "BLACKPANTHER", img: "http://pluspng.com/img-png/black-panther-png-image-cw-panther-kick-render-png-marvel-cinematic-universe-wiki-fandom-powered-by-wikia-1755.png"},
-    {hero: "VISION", img: "https://vignette.wikia.nocookie.net/marvelmovies/images/1/18/Vision_Avengers_Age_of_Ultron_Promo.png/revision/latest?cb=20150408003114"},
-    {hero: "THOR", img: "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/c/ca/Thor-AOU-Render.png/revision/20161114043212"},
-    {hero: "CAPTAINAMERICA", img: "https://vignette.wikia.nocookie.net/deathbattlefanon/images/f/f4/Captain_America.png/revision/latest?cb=20140816213102"}
+const marvelHerosArray = [{
+        hero: "ANTMAN",
+        img: "http://1.bp.blogspot.com/-bJSg0copQpc/VpvGvX3QTLI/AAAAAAAAA1o/DLX7V3GrIw8/s1600/Ant-Man.png"
+    },
+    {
+        hero: "IRONMAN",
+        img: "http://static.tumblr.com/5a7f2db3663f6b7a24edf8efa35c0b0f/povhbe1/JlHmn510g/tumblr_static_ironman_3.png"
+    },
+    {
+        hero: "HULK",
+        img: "https://pre00.deviantart.net/e795/th/pre/i/2016/274/7/8/hulk___transparent_by_asthonx1-dajha0a.png"
+    },
+    {
+        hero: "BLACKWIDOW",
+        img: "http://pluspng.com/img-png/black-widow-png-download-black-widow-png-images-transparent-gallery-advertisement-1473.png"
+    },
+    {
+        hero: "SPIDERMAN",
+        img: "https://purepng.com/public/uploads/large/purepng.com-amazing-spidermanspider-manspidermansuperherocomic-bookmarvel-comicscharacterstan-leegamesmovie-1701528655908xx8zi.png"
+    },
+    {
+        hero: "GROOT",
+        img: "https://pre00.deviantart.net/010b/th/pre/i/2018/026/1/0/gotg___groot__2__by_sidewinder16-dc17uti.png"
+    },
+    {
+        hero: "STARLORD",
+        img: "https://pre00.deviantart.net/1673/th/pre/i/2017/339/e/7/star_lord___infinity_war_png_by_natan_ferri-dbvtc3x.png"
+    },
+    {
+        hero: "BLACKPANTHER",
+        img: "http://pluspng.com/img-png/black-panther-png-image-cw-panther-kick-render-png-marvel-cinematic-universe-wiki-fandom-powered-by-wikia-1755.png"
+    },
+    {
+        hero: "VISION",
+        img: "https://pre00.deviantart.net/53e2/th/pre/f/2016/115/5/4/civil_war_vision___transparent_background__by_camo_flauge-da095bs.png"
+    },
+    {
+        hero: "THOR",
+        img: "http://pluspng.com/img-png/thor-hd-png-image-thor-2-avengers-fh-png-marvel-cinematic-universe-wiki-fandom-powered-by-wikia-1416.png"
+    },
+    {
+        hero: "CAPTAINAMERICA",
+        img: "http://pngimg.com/uploads/captain_america/captain_america_PNG43.png"
+    }
 ]
 
 const createRandomHero = () => {
-
+    const heroImageDiv = document.createElement('img')
     const randomHerolNumber = Math.floor(Math.random() * marvelHerosArray.length)
     const randomHero = marvelHerosArray[randomHerolNumber]
+    heroImageDiv.setAttribute("src", `${randomHero.img}`)
+    $('.countdown').append(`<img id= "heroImage" src=${randomHero.img}>`)
     return randomHero
 
 }
 
 
-var word = createRandomHero()
+var word = createRandomHero().hero
 console.log(word)
 
 $(() => {
+
     //take wordArr and insert divs = to length of wordArr
     for (let i = 0; i < word.length; i++) {
         $newDiv = $('<div></div>', {
@@ -34,7 +68,6 @@ $(() => {
         })
         $('.userLetters').append($newDiv)
     }
-
     //letters selected//
     $('.letterList').on('click', ($event) => {
         var letterSelected = $event.target.innerHTML
@@ -42,18 +75,16 @@ $(() => {
             $(`.${letterSelected}`).replaceWith(`<div>${letterSelected}</div>`)
         }
     })
-
-
     //losing function//
     $('.letterList').on('click', ($event) => {
         var letterSelect = $event.target.innerHTML
-        console.log(letterSelect)
         if (word.indexOf(letterSelect) === -1) {
             var life = $('.life').text()
             $('.life').html(`${life -= 1}`)
-
             if (life === 0) {
-                alert('get out my face loser')
+                $('.countdown').append($("<img>", {id: "heroImageLose", src:"https://vignette.wikia.nocookie.net/deathbattlefanon/images/0/06/Thanos.png/revision/latest/scale-to-width-down/2000?cb=20150701054527"}))
+                $('#heroImageLose').css("display", "initial")
+                $('.countdown').append("<div id='loseMessage'></div>")
             }
         }
     })
@@ -63,14 +94,7 @@ $(() => {
         var finishMessage = $('.finishMessage').text()
         if (letters == word) {
             $('.finishMessage').html("YOU WIN YOU BIG LOSER")
-            // alert("you win")
+            $('#heroImage').css("display", "initial")
         }
-    })
-
-
-    //styles
-
-    $('heroImage').on('load', () => {
-
     })
 })
