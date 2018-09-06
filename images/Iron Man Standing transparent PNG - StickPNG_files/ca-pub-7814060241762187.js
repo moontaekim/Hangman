00a8436dd245f49fa556ca6a7413b1a124915dad
Moon-Tae Kim <moon-tae@Moon-Taes-MacBook-Air.local>
2018-09-06 13:22:54 -0400

    basic styling done

---
+++ images/Iron Man Standing transparent PNG - StickPNG_files/ca-pub-7814060241762187.js
@@ -0,0 +1,1 @@
+try{window.localStorage.setItem('google_pub_config','{"sraConfigs":{"2":{"sraEnabled":true,"sraTimeout":60000,"whitelist":{"m78m5w,2":1,"1elihf0,2":1,"1tt9rpf,2":1,"1k3y81v,2":1,"1rk437,2":1,"d1r2el,2":1}},"4":{"sraEnabled":true,"sraTimeout":60000,"whitelist":{"d1r2el,2":1,"1h44wu3,2":1,"1elihf0,2":1,"m78m5w,2":1,"1tt9rpf,2":1,"1k3y81v,2":1,"1rk437,2":1}}}}');}catch(e){}
--- script.js
+++ script.js
@@ -6,67 +6,57 @@ const createRandomHero = () => {
     const randomHero = marvelHerosArray[randomHerolNumber]
     return randomHero
 
-
-}   
-
+}
 
 
 var word = createRandomHero()
 console.log(word)
 
 $(() => {
-
-    
-//take wordArr and insert divs = to length of wordArr
-    for(let i = 0; i < word.length; i++){
-        $newDiv = $('<div></div>', {'class': word[i], text:'_' } )
+    //take wordArr and insert divs = to length of wordArr
+    for (let i = 0; i < word.length; i++) {
+        $newDiv = $('<div></div>', {
+            'class': word[i],
+            text: '_'
+        })
         $('.userLetters').append($newDiv)
-    }  
+    }
 
- //letters selected//
-    $('.letterList').on('click', ($letterSelected) => {
-        var letterSelected = $letterSelected.target.innerHTML
-
-        if(word.includes(letterSelected) === true){
-            $(`.${letterSelected}`).replaceWith(`<div>${letterSelected}</div>`) 
-
-        }
-
-//losing function//
+    //letters selected//
     $('.letterList').on('click', ($event) => {
-        var letterSelect = $event.target.innerHTML
-        if(word.indexOf(letterSelect) === -1){
-            var life = $('.life').text()
-            $('.life').html(`${life -= 1}`)
-        
-        if (life === 0) {
-            alert('get out my face loser')
-
-        }}
+        var letterSelected = $event.target.innerHTML
+        if (word.includes(letterSelected) === true) {
+            $(`.${letterSelected}`).replaceWith(`<div>${letterSelected}</div>`)
+        }
     })
 
- //winning funciton//
+
+    //losing function//
+    $('.letterList').on('click', ($event) => {
+        var letterSelect = $event.target.innerHTML
+        console.log(letterSelect)
+        if (word.indexOf(letterSelect) === -1) {
+            var life = $('.life').text()
+            $('.life').html(`${life -= 1}`)
+
+            if (life === 0) {
+                alert('get out my face loser')
+            }
+        }
+    })
+    //winning funciton//
     $('.letterList').on('click', () => {
         var letters = $('.userLetters').text()
         var finishMessage = $('.finishMessage').text()
         if (letters == word) {
             $('.finishMessage').html("YOU WIN YOU BIG LOSER")
-           // alert("you win")
+            // alert("you win")
         }
     })
+
+
+    //styles
+
+    $('.userLetters').css('font-size')
+
 })
-
-
-    })
-
-
-
-
-
-
-
-
-
-
-
-
